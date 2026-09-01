f7f3c665f97b6056a4182d5dfbc78984bf9353c8
Dan Nelson <263006653+dgnelsonoz@users.noreply.github.com>
2026-09-01 15:24:25 +0800

    Match BIP39 lookup formatting style

--- common/bip39_lookup.c
+++ common/bip39_lookup.c
@@ -1,14 +1,16 @@
 #include "bip39_lookup.h"
 
-enum{ BIP39_WORD_COUNT = 2048 };
+enum { BIP39_WORD_COUNT = 2048 };
 
-static const char *wordlist[BIP39_WORD_COUNT] = {
+static const char *wordlist[ BIP39_WORD_COUNT ] =
+{
 #include "wordlists/english_words.c.inc"
 };
 
 const char *bip39_get_word_by_index(uint16_t index)
 {
-    if (index >= BIP39_WORD_COUNT) {
+    if( index >= BIP39_WORD_COUNT )
+    {
         return "ERR";
     }
 
